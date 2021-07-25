86bef0453e5f62ffe44888f1f079cc69ed76bf2a
eamon-b <>
2021-07-25 10:34:54 +1000

    Fixed getting onchain transactions

--- onchain_transactions.py
+++ onchain_transactions.py
@@ -1,35 +1,59 @@
 """ create a csv of onchain transaction """
 
+import requests
 import os
 
 from argparse import ArgumentParser
 
-from covalent_api.class_a import ClassA
-from covalent_api.session import Session
+
+# since the covalent api python bindings weren't authenticating properly,
+# here we just copy the relevant api calls, and edit them slightly to have key in the
+# request's parameters to try to get it to work
+
+def get_transactions(chain_id, address, api_key, block_signed_at_asc=True, no_logs=False):
+    '''
+    Retrieve all transactions for address including their decoded log events.
+    This endpoint does a deep-crawl of the blockchain to retrieve all kinds
+    of transactions that references the address.
+    '''
+
+    method_url = f'/v1/{chain_id}/address/{address}/transactions_v2/'
+
+    params = {
+        'block-signed-at-asc': block_signed_at_asc,
+        'no-logs': no_logs,
+        'format': 'csv',
+        'key': api_key
+    }
+
+    result = query(method_url, params)
+
+    return result
+
+def query(url, params=None):
+    '''
+    Query the *url* request with the given *params*
+
+    :param url: path url to query.
+    :param params: Dictionary with url parameters
+    :param decode: Json decode the returned response from the server.
+    '''
+    url = "{}{}".format('https://api.covalenthq.com', url)
+
+    response = requests.get(url, params=params)
+
+    result = response.text
+    if not result:
+        return result
+
+    return result
 
 def save_transactions(chain, address, api_key):
 
-    # make a Session with our api key
-    session = Session(api_key=api_key)
-
-    print(session.api_key)
-
-    # make the class with our session in order to query the api
-    A = ClassA(session)
-
     # mapping to translate the chain name into it's value
     chain_ids = {'ethereum': '1', 'polygon': '137', 'bsc': '56', 'fantom': '250'}
-    print(chain_ids[chain])
 
-    data_csv = A.get_transactions(
-        chain_ids[chain],
-        address,
-        block_signed_at_asc=True, # True to get results in ascending order
-        no_logs=False,
-        page_number=None,
-        page_size=None,
-        format='csv' # json is the other option
-    )
+    data_csv = get_transactions(chain_ids[chain], address, api_key)
 
     # save the data in the correct transaction-files subdirectory
     filename = os.path.join('transaction-files', chain, 'transactions.csv')
