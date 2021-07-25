eab56fc8a9a40d5b136b0ebad891d3a2efed80b8
amyandpotatoes <u5805193@anu.edu.au>
2021-07-25 15:01:20 +1000

    added filtering of onchain transactions before writing to csv

--- onchain_transactions.py
+++ onchain_transactions.py
@@ -3,14 +3,17 @@
 import requests
 import os
 
+import pandas as pd
+
 from argparse import ArgumentParser
+from io import StringIO
 
 
 # since the covalent api python bindings weren't authenticating properly,
 # here we just copy the relevant api calls, and edit them slightly to have key in the
 # request's parameters to try to get it to work
 
-def get_transactions(chain_id, address, api_key, block_signed_at_asc=True, no_logs=False):
+def get_transactions(chain_id, address, api_key, block_signed_at_asc=False, no_logs=False):
     '''
     Retrieve all transactions for address including their decoded log events.
     This endpoint does a deep-crawl of the blockchain to retrieve all kinds
@@ -22,13 +25,15 @@ def get_transactions(chain_id, address, api_key, block_signed_at_asc=True, no_lo
     params = {
         'block-signed-at-asc': block_signed_at_asc,
         'no-logs': no_logs,
-        'format': 'csv',
-        'key': api_key
+        'format': 'json',
+        'key': api_key,
+        'page-size': 500,
     }
 
     result = query(method_url, params)
 
     return result
+
 
 def query(url, params=None):
     '''
@@ -42,18 +47,49 @@ def query(url, params=None):
 
     response = requests.get(url, params=params)
 
-    result = response.text
-    if not result:
-        return result
+    if response.json()['data']['pagination']['has_more']:
+        raise(Exception('Too many transactions, increase page size.'))
+
+    params['format'] = 'csv'
+
+    response_csv = requests.get(url, params=params)
+
+    result = response_csv.text
 
     return result
+
+
+def filter_transactions(result, address):
+
+    # read in transactions to pandas dataframe
+    df = pd.read_csv(StringIO(result), dtype=str)
+
+    df["from_address"] = df["from_address"].str.lower()
+    df["to_address"] = df["to_address"].str.lower()
+    df["log_events_decoded_params_value"] = df["log_events_decoded_params_value"].str.lower()
+
+    # only get lines which involve your wallet address and are not approvals
+    filtered_df = df[((df["from_address"] == address.lower())
+                      | (df["to_address"] == address.lower())
+                      | (df["log_events_decoded_params_value"] == address.lower()))
+                     & (df["log_events_decoded_signature"] != "Approval(indexed address owner, indexed address spender, uint256 value)")]
+
+    # convert pandas df into csv string
+    filtered_result = filtered_df.to_csv()
+
+    return filtered_result
+
 
 def save_transactions(chain, address, api_key):
 
     # mapping to translate the chain name into it's value
     chain_ids = {'ethereum': '1', 'polygon': '137', 'bsc': '56', 'fantom': '250'}
 
-    data_csv = get_transactions(chain_ids[chain], address, api_key)
+    # pull all transaction data
+    data_text = get_transactions(chain_ids[chain], address, api_key)
+
+    # filter transaction data to only get necessary lines
+    data_csv = filter_transactions(data_text, address)
 
     # save the data in the correct transaction-files subdirectory
     filename = os.path.join('transaction-files', chain, 'transactions.csv')
@@ -61,9 +97,10 @@ def save_transactions(chain, address, api_key):
     with open(filename, 'w') as file:
         file.write(data_csv)
 
+
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('chain', choices=['etherium', 'polygon', 'bsc', 'fantom'],
+    parser.add_argument('chain', choices=['ethereum', 'polygon', 'bsc', 'fantom'],
         help='The chain on which to get all ')
     parser.add_argument('address', help='Your wallet address')
     parser.add_argument('api_key', help='Your covalent api key')
